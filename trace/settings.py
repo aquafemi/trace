9203d5c0b164d8c3ab448a9ed857b7edf5250958
Femi Olukoya <oolukoya@andrew.cmu.edu>
2016-02-20 17:28:35 -0500

    changed debug to false

--- trace/settings.py
+++ trace/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+4zicqdp7y!*#14z*xah#zid#!m*y903qnz(+7q0xzgp54$od6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'traced.herokuapp.com']
 
