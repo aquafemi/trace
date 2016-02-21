1b076d65a6ec05cb63d712104ebdb172cbd0a980
Femi Olukoya <oolukoya@andrew.cmu.edu>
2016-02-20 19:00:44 -0500

    Migrated to postgres backend

--- trace/settings.py
+++ trace/settings.py
@@ -103,7 +103,7 @@ WSGI_APPLICATION = 'trace.wsgi.application'
 DATABASES = {}
 DATABASES['default'] =  dj_database_url.config()
 DATABASES['default'].update({'ENGINE': 'django.db.backends.postgresql_psycopg2',
-'NAME': 'trace'})
+'NAME': 'traced'})
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
